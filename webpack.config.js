d2c3ad4715c50d173f7f7788a3d8a2914c3fb71f
ddobi <qhdqhdekd261@naver.com>
2022-07-22 11:16:50 +0900

    end of lecture

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,18 @@ module.exports = {
   output: {
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
   // 번들링 후 결과물의 처리 방식 등 다양한 플로그인들을 설정
   plugins: [
     new HtmlPlugin({
